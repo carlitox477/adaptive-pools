0b8df417f08ff7606940e3ff868ebb1758dd6b51
Ðavid <info.dcm62@gmail.com>
2024-03-16 15:56:07 +0100

    Update SimpleTStore.sol
    Natspec

--- src/SimpleTStore.sol
+++ src/SimpleTStore.sol
@@ -1,12 +1,24 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.20;
 
+/// @title SimpleTStore
+/// @dev This contract demonstrates the use of EVM's `tstore` and `tload` instructions for direct storage access.
 contract SimpleTStore {
+    
+    /// @notice Stores a value at a specified storage slot.
+    /// @dev Directly writes a value to storage using inline assembly and the `tstore` opcode.
+    /// @param key The storage slot to write to.
+    /// @param value The value to store at the specified slot.
     function tstore(uint key, uint value) external {
         assembly {
             tstore(key, value)
         }
     }
+
+    /// @notice Loads a value from a specified storage slot.
+    /// @dev Reads a value from storage using inline assembly and the `tload` opcode.
+    /// @param key The storage slot to read from.
+    /// @return value The value stored at the specified slot.
     function tload(uint key) external view returns (uint value) {
         assembly {
             value := tload(key)
