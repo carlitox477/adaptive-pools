466b6b3c337f27fe83787e0f7a4972304b59deec
pm <misirov@pablos-MacBook-Air.local>
2024-03-16 01:55:20 +0000

    downgrade v4 solidity version

--- test/PoolDeployment.t.sol
+++ test/PoolDeployment.t.sol
@@ -5,6 +5,13 @@ import "forge-std/Test.sol";
 
 import "src/mock/MockToken.sol";
 
+// Import necessary contract for deployment
+import "v4-core/PoolManager.sol";
+import {CurrencyLibrary, Currency} from "v4-core/types/Currency.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {IHooks} from "v4-core/interfaces/IHooks.sol";
+import "v4-core/interfaces/IPoolManager.sol";
+import "v4-core/types/PoolKey.sol";
 
 
 
@@ -12,16 +19,23 @@ contract PoolDeployment is Test {
 
     ERC20 token0;
     ERC20 token1;
+    PoolManager pool;
 
     function setUp() public {
         token0 = new MockToken("weth", "WETH");
         token1 = new MockToken("usdc", "USDC");
 
+        pool = new PoolManager(123);
     }
 
     function test_token() public view {
         assertEq(token0.totalSupply(), 1000000 * 10**18);
     }
+
+    function test_PoolDeployed() public {
+        pool.MAX_TICK_SPACING();
+    }
+
 
 
 
